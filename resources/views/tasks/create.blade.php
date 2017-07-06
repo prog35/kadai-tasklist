964a0d222eed2f570d2e2c15f9cdebd51bec7da1
taka8 <learning.prog35@gmail.com>
2017-07-06 20:29:03 +0000

    lesson9-8 kadai created

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスクの追加ページ</h1>
+    
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        {!! Form::label('content', 'タスク：') !!}
+        {!! Form::text('content') !!}
+        
+        {!! Form::submit('作成') !!}
+        
+    {!! Form::close() !!}
+        
+@endsection

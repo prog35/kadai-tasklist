07084f0e7931142a6a2bc62340cef2f03de3d8c3
taka8 <learning.prog35@gmail.com>
2017-07-08 21:11:35 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,6 +5,9 @@
     <h1>タスクの追加ページ</h1>
     
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+ 
+        {!! Form::label('status', '状態：') !!}
+        {!! Form::text('status') !!}
     
         {!! Form::label('content', 'タスク：') !!}
         {!! Form::text('content') !!}
